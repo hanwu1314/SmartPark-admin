d6c3f3b4bea58e3cbf2d99e04e9886295c660e10
hanwu <hanwu1314@outlook.com>
2023-09-21 20:11:37 +0800

    git init

---
+++ src/layout/components/Sidebar/Logo.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="sidebar-logo-container">
+    <h3 class="title">智慧园区</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SidebarLogo'
+}
+</script>
+
+<style lang="scss" scoped>
+
+.sidebar-logo-container {
+  position: relative;
+  width: 100%;
+  height: 60px;
+  line-height: 60px;
+  overflow: hidden;
+  .title{
+    text-align: center;
+    letter-spacing: 10px;
+  }
+}
+</style>
