133f7953e22d579f845227839c743d8f1271168e
hanwu <hanwu1314@outlook.com>
2023-09-25 12:15:05 +0800

    楼宇管理-列表数据渲染

--- src/views/Park/Building/index.vue
+++ src/views/Park/Building/index.vue
@@ -1,7 +1,86 @@
 <template>
-  <div class="building-container">楼宇管理</div>
+  <div class="building-container">
+    <!-- 搜索区域 -->
+    <div class="search-container">
+      <div class="search-label">楼宇名称：</div>
+      <el-input placeholder="请输入内容" class="search-main" />
+      <el-button type="primary">查询</el-button>
+    </div>
+    <!-- 表格区域 -->
+    <div class="table">
+      <el-table style="width: 100%" :data="buildingList">
+        <el-table-column type="index" label="序号" />
+        <el-table-column label="楼宇名称" width="180" prop="name" />
+        <el-table-column label="层数" prop="floors" />
+        <el-table-column label="在管面积(m²)" width="120" prop="area" />
+        <el-table-column
+          label="物业费(元/m²)"
+          width="120"
+          prop="propertyFeePrice" />
+        <el-table-column label="状态" prop="status">
+          <template #default="scope">
+            {{ formatStatus(scope.row.status) }}
+          </template>
+        </el-table-column>
+        <el-table-column label="操作" width="120">
+          <template #default="scope">
+            <el-button size="mini" type="text">编辑</el-button>
+            <el-button size="mini" type="text">删除</el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+    </div>
+  </div>
 </template>
 
-<script></script>
+<script>
+import { getBuildingListAPI } from '@/services'
+export default {
+  name: 'Building',
+  data() {
+    return {
+      buildingList: [], // table列表
+      params: {
+        // 请求参数
+        page: 1,
+        pageSize: 10
+      }
+    }
+  },
+  mounted() {
+    this.getBuildingList()
+  },
+  methods: {
+    async getBuildingList() {
+      const res = await getBuildingListAPI(this.params)
+      this.buildingList = res.data.rows
+    },
+    formatStatus(status) {
+      const statusMap = {
+        0: '租赁中',
+        1: '闲置中'
+      }
+      return statusMap[status]
+    }
+  }
+}
+</script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.department-container {
+  padding: 10px;
+}
+.search-container {
+  display: flex;
+  align-items: center;
+  border-bottom: 1px solid rgb(237, 237, 237, 0.9);
+  padding-bottom: 20px;
+  .search-label {
+    width: 100px;
+  }
+  .search-main {
+    width: 220px;
+    margin-right: 10px;
+  }
+}
+</style>
